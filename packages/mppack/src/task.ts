3e0ff70cdd4d52c9f134e4567cf5e08c46ff54f6
hufeng <hufeng@qianmi.com>
2018-12-05 15:09:16 +0800

    add images support and auto minify image

--- packages/mppack/src/task.ts
+++ packages/mppack/src/task.ts
@@ -1,10 +1,18 @@
 import del from 'del';
 import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
 import ts from 'gulp-typescript';
 import config from './config';
 
 export const clean = () => {
   return del(config.output);
+};
+
+export const image = () => {
+  return gulp
+    .src(config.image)
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.output));
 };
 
 export const typescript = () => {
@@ -17,6 +25,7 @@ export const typescript = () => {
 
 export const watch = () => {
   gulp.watch(config.typescript, typescript);
+  gulp.watch(config.image, image);
 };
 
-export const build = gulp.series(clean, gulp.parallel(typescript));
+export const build = gulp.series(clean, gulp.parallel(typescript, image));
